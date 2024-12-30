5f00ec61e4ad1a815be68c204814c911a5a9f053
Juan Pablo Silva <juanpablo@eduti.cl>
2024-12-30 10:06:51 -0300

    MENSAJE DE PRUEBA

--- PISE-APP/settings.py
+++ PISE-APP/settings.py
@@ -2,7 +2,7 @@ import os
 import dj_database_url
 
 # Asegúrate de que ROOT_URLCONF apunte correctamente
-ROOT_URLCONF = 'PISE-APP.urls'
+ROOT_URLCONF = 'pise_app.urls'
 
 # Y también el WSGI_APPLICATION
-WSGI_APPLICATION = 'PISE-APP.wsgi.application' 
+WSGI_APPLICATION = 'pise_app.wsgi.application' 
